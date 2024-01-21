21f318d188df03e0bd0ad17a251a3716d58b5457
Isiah Jones <jones.isiah.blhs@gmail.com>
2024-01-20 22:11:41 -0500

    Add function to create guid

--- store/boardStore.ts
+++ store/boardStore.ts
@@ -9,11 +9,23 @@ export const useBoardStore = defineStore("boardStore", () => {
     board.value.columns.push({
       name: columnName,
       tasks: [],
+      id: generateGUID(),
     });
   }
 
   function deleteColumn(columnIndex: number) {
     board.value.columns.splice(columnIndex, 1);
+  }
+
+  function generateGUID() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
   }
 
   return {
